5eb5da508f78d6ad5bdb4968811ed1b0603eb230
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-26 21:39:11 +0530

    Add files via upload

---
+++ Exception handling2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exception_handling_1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            try
+            {
+                throw new DivideByZeroException("Invalid Division");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exception");
+            }
+            Console.WriteLine("LAST STATEMENT");
+            Console.ReadKey();
+        }
+    }
+}
+        
+
+
