69f90a3b1e0113bc44109193ef9221ded105d61f
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-26 21:59:45 +0530

    Add files via upload

---
+++ User defined/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace User_defined_Exception
+{
+    class Program
+    {
+        class MyException : ApplicationException 
+        {
+            public MyException()
+            {
+                Console.WriteLine("User defined exception");
+            }
+        }
+        class MyClient
+        {
+            public static void Main()
+            {
+                try
+                {
+                    throw new MyException();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception caught here" + e.ToString());
+                }
+                Console.WriteLine("LAST STATEMENT");
+            }
+        }
+    }
+}
+
+
+  
+    
+
